b97bed2d29050ddab97fe69db60e8e179f7efd25
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-04-17 23:10:22 -0600

    Initial Assignment

---
+++ Likes_Part_1.js
@@ -0,0 +1,7 @@
+function count(){
+let counter = document.querySelector("#likeCount");
+console.log(counter.innerText)
+counter.innerText++
+}
+
+
---
+++ Likes_Part_2.js
@@ -0,0 +1,18 @@
+function count1(){
+    let counter = document.querySelector("#likeCount1");
+    console.log(counter.innerText)
+    counter.innerText++
+    }
+
+function count2(){
+    let counter = document.querySelector("#likeCount2");
+    console.log(counter.innerText)
+    counter.innerText++
+}
+
+function count3(){
+    let counter = document.querySelector("#likeCount3");
+    console.log(counter.innerText)
+    counter.innerText++
+}
+
